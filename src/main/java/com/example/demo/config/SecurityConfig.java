0f829cd9ce50d875496795cd001f5cbbc9b60b0a
bala-amypo <bala@amypo.in>
2025-12-26 05:38:06 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**","swagger-ui.html").permitAll()
                 .requestMatchers("/status").permitAll()
                 .requestMatchers("/hello-servlet").permitAll()
                 .requestMatchers("/api/**").authenticated()
